b72e4b0d810420b72c189449d0528edc88ca4a62
djemil <djemilismaili@web.de>
2025-12-28 20:33:27 +0100

    Add JobApplication model and ApplicationStatus enum

---
+++ JobTracker/Models/ApplicationStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace JobTracker.Models
+{
+    public enum ApplicationStatus
+    {
+        Applied = 0,
+        Interview = 1,
+        Offer = 2,
+        Rejected = 3
+    }
+}
---
+++ JobTracker/Models/JobApplication.cs
@@ -0,0 +1,12 @@
+﻿namespace JobTracker.Models
+{
+    public class JobApplication
+    {
+        public int Id { get; set; }
+        public required string CompanyName { get; set; }
+        public required string JobTitle { get; set; }
+        public DateTime ApplicationDate { get; set; }
+        public ApplicationStatus Status { get; set; }
+        public string? Notes { get; set; }
+    }
+}
