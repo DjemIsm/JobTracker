1b4e6e3ffa5f880ade579acfe6ea14b87799d7b3
djemil <djemilismaili@web.de>
2025-12-28 22:22:33 +0100

    Configure ApplicationDbContext with SQLite

--- JobTracker/Program.cs
+++ JobTracker/Program.cs
@@ -1,6 +1,15 @@
+using JobTracker.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString(
+    "DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(connectionString));
+
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
