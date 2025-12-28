15ead3fb8f19f30608ece900a3836339fec1fd62
djemil <djemilismaili@web.de>
2025-12-29 00:01:18 +0100

    Add JobApplications index page

---
+++ JobTracker/Controllers/JobApplicationsController.cs
@@ -0,0 +1,22 @@
+﻿using JobTracker.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobTracker.Controllers
+{
+    public class JobApplicationsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public JobApplicationsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var jobApplications = await _context.JobApplications.ToListAsync();
+            return View(jobApplications);
+        }
+    }
+}
