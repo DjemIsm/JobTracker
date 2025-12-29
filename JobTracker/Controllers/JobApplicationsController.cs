927531a2243f1f2ac1c9808683c8122e6ec4d5f0
djemil <djemilismaili@web.de>
2025-12-29 15:27:27 +0100

    Fix create form layout

--- JobTracker/Controllers/JobApplicationsController.cs
+++ JobTracker/Controllers/JobApplicationsController.cs
@@ -1,4 +1,5 @@
 ﻿using JobTracker.Data;
+using JobTracker.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,6 +18,24 @@ namespace JobTracker.Controllers
         {
             var jobApplications = await _context.JobApplications.ToListAsync();
             return View(jobApplications);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(JobApplication jobApplication)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(jobApplication);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(jobApplication);
         }
     }
 }
--- JobTracker/Models/JobApplication.cs
+++ JobTracker/Models/JobApplication.cs
@@ -1,12 +1,26 @@
-﻿namespace JobTracker.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobTracker.Models
 {
     public class JobApplication
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        [Display(Name = "Firmenname")]
         public required string CompanyName { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        [Display(Name = "Jobtitel")]
         public required string JobTitle { get; set; }
-        public DateTime ApplicationDate { get; set; }
-        public ApplicationStatus Status { get; set; }
+
+        [Display(Name = "Bewerbungsdatum")]
+        public DateTime ApplicationDate { get; set; } = DateTime.Today;
+        public ApplicationStatus Status { get; set; } = ApplicationStatus.Applied;
+        [StringLength(2000)]
+        [Display(Name = "Notizen")]
         public string? Notes { get; set; }
     }
 }
