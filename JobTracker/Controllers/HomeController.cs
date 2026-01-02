3d14c54e5ebf88eca0155d4d82b24880bdd078a4
djemil <djemilismaili@web.de>
2026-01-02 22:15:04 +0100

    Add dashboard with status counts and recent job applications

--- JobTracker/Controllers/HomeController.cs
+++ JobTracker/Controllers/HomeController.cs
@@ -1,32 +1,58 @@
-using System.Diagnostics;
+using JobTracker.Data;
 using JobTracker.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace JobTracker.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
+        
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var groupedData = await _context.JobApplications
+                .GroupBy(x => x.Status)
+                .Select(g => new { Status = g.Key, Count = g.Count() })
+                .ToListAsync();
+
+            var statusCounts = groupedData.ToDictionary(x => x.Status, x => x.Count);
+
+            foreach (ApplicationStatus status in Enum.GetValues(typeof(ApplicationStatus)))
+            {
+                if (!statusCounts.ContainsKey(status))
+                {
+                    statusCounts[status] = 0;
+                }
+            }
+
+            // Kürzlich hinzugefügte Bewerbungen (z.B. die letzten 5)
+            var recentApplications = await _context.JobApplications
+                .OrderByDescending(x => x.ApplicationDate)
+                .ThenByDescending(x => x.Id)
+                .Take(5)
+                .ToListAsync();
+
+            var dashboardViewModel = new DashboardViewModel
+            {
+                ApplicationStatusCounts = statusCounts,
+                RecentApplications = recentApplications
+            };
+
+            return View(dashboardViewModel);
         }
 
         public IActionResult Privacy()
         {
             return View();
         }
-
-        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        //public IActionResult Error()
-        //{
-        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        //}
     }
 }
---
+++ JobTracker/Models/DashboardViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace JobTracker.Models
+{
+    public class DashboardViewModel
+    {
+        public Dictionary<ApplicationStatus, int> ApplicationStatusCounts { get; set; } = new();
+        public List<JobApplication> RecentApplications { get; set; } = new();
+    }
+}
